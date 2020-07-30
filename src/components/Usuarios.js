a80b7cc3c911c88fe8f2e0283e6917ee3ba7f1e6
TomasArdiani <62221771+TomasArdiani@users.noreply.github.com>
2020-07-30 19:19:13 -0300

    comentanding

---
+++ src/components/Usuarios.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../css/Usuarios.css';
+
+function Usuarios() {
+    return (
+        <div class="row container-usuarios col-6" style={{padding: 0}}>
+           <h2 class="h2-usuarios" style={{padding: 10}}>Usuarios </h2>
+           <h4 class= "h4-cant-usuarios" style={{padding: 10}}>89</h4>
+            </div>
+        )
+    }
+    
+    export default Usuarios;
